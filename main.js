26b49818964e4b7dd0d9c472032c2d40abbf5e72
cubeeso <cubees@foxmail.com>
2019-11-23 21:32:50 +0800

    新增标题栏

--- main.js
+++ main.js
@@ -2,26 +2,48 @@ import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js
 import Storage from "./router/storage.js"
 //基础输入组件
 const BaseInput = {
-    template: `<div class="ui-input" ><input placeholder="请输入" v-focus  v-model="it" @keyup.enter="$emit('put-event',it,$event.target)" /></div>`,
+    template: /*html*/ `
+      <div class="ui-input" >
+            <input placeholder="请输入" v-focus  v-model="it" @keyup.enter="$emit('put-event',it,$event.target)" />
+    </div>`,
     data() {
         return {
             it: "",
         };
     },
-    directives:{
-        focus:{
-            inserted(el){
-                el.focus(); //等同于autofocus 在苹果浏览器是无效
+    directives: {
+        focus: {
+            inserted(el) {
+                el.focus(); //等同于autofocus    可兼容苹果浏览器是无效
             }
         }
     }
 };
 //基础list组件
 const BaseList = {
-    template: `<div class="ui-list"> <ol >
-            <li v-for="(item,index) in items" :key="index" ><span @click="$emit('copy-event',item)">复制</span><span>{{item}}</span> <span @click="$emit('remove-event',index)">删除</span></li>
-             </ol> </div >`,
+    template: /*html*/ `
+        <div class="ui-list">
+             <p v-for="(item,index) in items" :key="index" >
+                <span>{{item}}</span>
+                <span class="ui-icon ui-icon-copy" @click="$emit('copy-event',item)"></span>
+                <span class="ui-icon ui-icon-del" @click="$emit('remove-event',index)"></span>
+               </p>
+         </div >`,
     props: ["items"]
+};
+//标题栏
+const TitleBar = {
+    template:/*html*/`
+        <div class="ui-bar">
+             <span title="菜单" class="ui-icon ui-icon-menu"></span>
+             <span title="更多" class="ui-icon ui-icon-more" v-on="$listeners"  @click="showBuble" ></span>
+        </div>
+    `,
+    methods: {
+       showBuble(){//打开气泡
+             this.$root.$children[0].$refs.tips.isBub = !this.$root.$children[0].$refs.tips.isBub;
+       }
+    },
 };
 //混入的钩子函数
 const mix = {
@@ -32,37 +54,74 @@ const mix = {
         });
     }
 };
-//轻提示组件
-const Toast = {
-    template: `<transition name="fade"><div class="ui-toast" v-show="conf.isShow" >{{conf.msg}}</div></transition>`,
-    props: ["conf"]
+//提示组件
+const Tips = {
+    template:/*html*/ `
+        <div class="ui-tips">
+            <transition name="fade">
+             <div class="ui-toast" v-show="isToast" >{{Amsg}}</div>
+            </transition>
+            <transition name="show">
+                <div class="ui-box ui-alert" v-show="isAlert" >
+                 <p>{{Bmsg}}</p>
+                 <p><button @click="closeAlert">确定</button></p>       
+                </div>
+            </transition>
+            <transition name="show">
+                <div class="ui-box ui-buble" v-show="isBub" >
+                    <span title="复制缓存" class="ui-icon ui-icon-copy" @click="$emit('copyall-event')"></span>
+                    <span title="清空缓存" class="ui-icon ui-icon-del"  @click="$emit('removeall-event')"></span>
+                </div>
+             </transition>
+        </div>`,
+    //props: ["conf"]
+    data(){
+        return {
+            isToast:false,
+            isAlert:false,
+            isBub:false,
+            Amsg:"",
+            Bmsg:""
+        }
+    },
+    methods: {
+        closeAlert(){
+            this.isAlert = false;
+        }
+    },
 };
 //主App
 const App = {
-    template: `<div id="app" >
-        <Toast :conf="conf"></Toast>
-		<h2><span @click="copyAll">复制缓存</span><span @click="removeAll">清除缓存</span></h2>
-        <h1>key in something</h1>
-        <BaseInput @put-event="put"></BaseInput>
-        <BaseList :items="items" v-on="{'remove-event':remove,'copy-event':copy}"></BaseList>
-    </div>`,
+    template:/*html*/ `
+        <div id="app" >
+            <div class="head">
+                <TitleBar v-on="{'copyall-event':copyAll,'removeall-event':removeAll}"></TitleBar>
+            </div>
+            <div class="content">
+                <Tips ref="tips"></Tips>
+                <h1>key in some thing</h1>
+                <BaseInput @put-event="put"></BaseInput>
+                <BaseList :items="items" v-on="{'remove-event':remove,'copy-event':copy}"></BaseList>
+            </div>
+            <div class="foot"> 我是底栏</div>
+        </div>`,
     data() {
         return {
             items: Storage.getCache() || [],
-            conf: {
-                isShow: false,
-                msg: ""
-            }
         }
     },
     methods: {
         showToast(msg) {
-            this.conf.msg = msg;
-            this.conf.isShow = true; //打开提示窗
-            clearTimeout(this.$data.t);
-            this.$data.t = setTimeout(() => {
-                this.conf.isShow = false; //关闭提示窗
+            this.$refs.tips.Amsg = msg;
+            this.$refs.tips.isToast = true; //打开提示窗
+            clearTimeout(this.$data.to);
+            this.$data.to = setTimeout(() => {
+                this.$refs.tips.isToast = false; //关闭提示窗
             }, 2000);
+        },
+        showAlert(msg){
+            this.$refs.tips.Bmsg = msg;
+            this.$refs.tips.isAlert = true; //打开提示窗
         },
         remove(index) {
             this.items.splice(index, 1);
@@ -70,8 +129,8 @@ const App = {
         },
         put(item, _this) {
             _this.value = "";
-            if(this.items.includes(item)){
-                this.showToast("数据已经存在");
+            if (this.items.includes(item)) {
+                this.showAlert("数据已经存在");
                 return;
             }
             this.items.push(item);
@@ -83,32 +142,33 @@ const App = {
         },
         removeAll() {
             if (this.items.length == 0) {
-                this.showToast("没有数据");
+                this.showAlert("没有数据");
                 return;
             }
             this.items = [];
-            this.showToast("清空缓存成功");
+            this.showAlert("清空缓存成功");
         },
         copyAll() {
             if (this.items.length == 0) {
-                this.showToast("没有数据");
+                this.showAlert("没有数据");
                 return;
             }
             if (this.items.length > 2000) {
-                this.showToast("缓存数据过大,无法复制");
+                this.showAlert("缓存数据过大,无法复制");
                 return;
             }
             this.items = [...this.items, ...this.items];
-            this.showToast("复制缓存成功");
+            this.showAlert("复制缓存成功");
         }
     },
     watch: {
-        
+
     },
     components: {
         BaseList,
         BaseInput,
-        Toast
+        Tips,
+        TitleBar
     },
     mixins: [mix]
 };
